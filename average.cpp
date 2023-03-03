d160a453e0715f57a5b6813609b9d2ad25f27ba0
Bank <sirawit.36298@gmail.com>
2023-03-03 15:04:16 +0700

    average completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+    double x;
+    double sum;
+    for(int i = 0;i<(argc-1);i++){
+        x += atoi(argv[i+1]);
+    }
+    sum = x/(argc-1);
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }else{
+        cout << "---------------------------------\n" << "Average of " << argc-1 <<  " numbers = " << sum << endl << "---------------------------------";
+    }
+}
